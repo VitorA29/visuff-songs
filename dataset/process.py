c6ec66bd26181d3931894358094265cfa84b4a03
VitorA29 <vitor_araujo.29@outlook.com>
2019-06-13 01:14:41 -0300

    Improuved the visualization, now can be filtered and agruped

--- dataset/process.py
+++ dataset/process.py
@@ -36,38 +36,14 @@ def main():
                         table.append(data.copy())
                         break
         # create the result dict
-        result = dict()
         songsList = list()
-        playlistList = dict()
+        playlistList = list()
         for row in table:
-            # get the playlist node
-            if row["playlist"] not in result:
-                result[row["playlist"]] = dict()
-                result[row["playlist"]]["playlist_name"] = row["playlist"]
-                result[row["playlist"]]["artistsD"] = dict()
-            playlist = result[row["playlist"]]
-
             # get the playlist songs array
-            if row["playlist"] not in playlistList:
-                playlistList[row["playlist"]] = list()
-            playlistSongs = playlistList[row["playlist"]]
-
-            # get the artist node
-            if row["artist_name"] not in playlist["artistsD"]:
-                playlist["artistsD"][row["artist_name"]] = dict()
-                playlist["artistsD"][row["artist_name"]]["artist_name"] = row["artist_name"]
-                playlist["artistsD"][row["artist_name"]]["albumsD"] = dict()
-            artist = playlist["artistsD"][row["artist_name"]]
-
-            # get the album node
-            if row["album_names"] not in artist["albumsD"]:
-                artist["albumsD"][row["album_names"]] = dict()
-                artist["albumsD"][row["album_names"]]["album_name"] = row["album_names"]
-                artist["albumsD"][row["album_names"]]["songs"] = list()
-            album = artist["albumsD"][row["album_names"]]
+            if row["playlist_name"] not in playlistList:
+                playlistList.append(row["playlist_name"])
 
             # insert the song
-            song_entry = dict()
             song_info = dict()
             for key in row:
                 # correct the data value
@@ -79,43 +55,15 @@ def main():
                     value = row[key]
                     
                 # populate the songs array
-                if key == "playlist":
-                    song_info["playlist_name"] = value
-                # elif key == "album_names":
-                #     song_info["album_name"] = value
-                elif key not in ["artist_name", "album_names"]:
+                if key not in [ "artist_name", "album_name" ]:
                     song_info[key] = value
-                
-                # populate the playlist array
-                if key not in ["artist_name", "album_names", "playlist"]:
-                    song_entry[key] = value
-            album["songs"].append(song_entry)
-            playlistSongs.append(song_entry)
             songsList.append(song_info)
 
-        # count the playlist
-        # result["#count"] = len(result.keys())
-        resultList = list()
-        for playlist in result.values():
-            if type(playlist) == int:
-                continue
-            playlist["artists"] = list()
-            for artist in playlist["artistsD"].values():
-                artist["albums"] = list()
-                for album in artist["albumsD"].values():
-                    artist["albums"].append(album)
-                artist.pop("albumsD")
-                playlist["artists"].append(artist)
-            playlist.pop("artistsD")
-            resultList.append(playlist)
-
         # export the json
-        with open("dataProcessed.json", 'w', encoding='utf-8') as f:
-            json.dump(resultList, f, sort_keys=True)
         with open("dataArray.json", 'w', encoding='utf-8') as f:
             json.dump(songsList, f , sort_keys=True)
         with open("dataKey.json", 'w', encoding='utf-8') as f:
-            json.dump(playlistList, f, indent=4 , sort_keys=True)
+            json.dump(playlistList, f, sort_keys=True)
 
 if __name__ == "__main__":
 	 main()
